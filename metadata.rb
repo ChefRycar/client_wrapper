b642effda76b49399f81f57612c6ac3c1fe1a285
Nick Rycar <rycar@chef.io>
2017-08-09 18:35:36 -0700

    dokken copypasta

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'nrycar@chef.io'
 license 'All Rights Reserved'
 description 'Installs/Configures client_wrapper'
 long_description 'Installs/Configures client_wrapper'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
